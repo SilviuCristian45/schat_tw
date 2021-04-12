5c2e7c3a92d669bd0fa8d766974c17d090fcc20a
DESKTOP-LDQLJQC\Silviu <silviudinca412@gmail.com>
2021-04-12 11:48:19 +0300

    Responsive UI + basic serverside index

--- js/chatglobal.js
+++ js/chatglobal.js
@@ -1,23 +1,23 @@
-console.log("chat global");
 let chatbox = document.getElementById("chatsection"); 
 
+//repetam acest request la fiecare 5 secunde si luam asincron din baza de date mesajele 
 setInterval( () => {
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // Typical action to be performed when the document is ready:
-        chatbox.innerHTML = this.response;
-    }
+    xhttp.onreadystatechange = function() {//la terminarea request-ului asignam functia aceasta
+        if (this.readyState == 4 && this.status == 200) {//daca avem ok-ul de la server ca s-a primit request-ul
+            chatbox.innerHTML = this.response;//punem raspunsul (lista de mesaje) in chatbox
+        }
     };
-    xhttp.open("GET", "server/messages.php", true);
-    xhttp.send();
+    xhttp.open("GET", "server/messages.php", true);//deschidem request-ul 
+    xhttp.send();//trimitem request-ul la server
 } , 5000)
 
-
+//request la click pe butonul send prin care inseram mesajul in baza de date 
 $("button").click(function(){
-    $.post("server/sendmessage.php",
+    $.post("server/sendmessage.php",//exact ca metoda de mai sus dar cu jquery
         {
-            message: $("textarea").val(),
+            message: $("textarea").val(),//punem mesajul scris in textarea 
+            //incarcam data si ora la care a fost trimis 
             timestampp: new Date().toISOString().slice(0, 19).replace('T', ' ')
         },
     );
--- server/sendmessage.php
+++ server/sendmessage.php
@@ -4,14 +4,10 @@
     $msgContent = $_POST["message"];
     $msgTimestamp = $_POST["timestampp"];
 
-    //echo $msgContent . " " . $msgTimestamp;
-
     $sql = "INSERT INTO messages(userfrom, timestampp,content) VALUES (1,'$msgTimestamp','$msgContent')";
     try {
         mysqli_query($conn, $sql);
-        echo "***";
     } catch (Exception $th) {
         echo $th;
-        echo "...";
     }
 ?>
