7795f9a5136d0e243a4e4182c5f345e55ff9abd3
DESKTOP-LDQLJQC\Silviu <silviudinca412@gmail.com>
2021-04-27 15:54:03 +0300

    Fixare bug de trimitere pe chatul global

--- server/sendmessage.php
+++ server/sendmessage.php
@@ -1,10 +1,12 @@
 <?php
     require 'config.php';
+    session_start();
 
+    $currentUser = $_SESSION["userid"];
     $msgContent = $_POST["message"];
     $msgTimestamp = $_POST["timestampp"];
 
-    $sql = "INSERT INTO messages(userfrom, timestampp,content) VALUES (1,'$msgTimestamp','$msgContent')";
+    $sql = "INSERT INTO messages(userfrom, timestampp,content) VALUES ('$currentUser','$msgTimestamp','$msgContent')";
     try {
         mysqli_query($conn, $sql);
     } catch (Exception $th) {
