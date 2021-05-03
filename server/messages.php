f74ba705b078ddfab24003ffb306485938dfe6e2
DESKTOP-LDQLJQC\Silviu <silviudinca412@gmail.com>
2021-05-03 15:47:34 +0300

    Mesaje colorate diferit pt admin si moderator

--- server/messages.php
+++ server/messages.php
@@ -1,7 +1,7 @@
 <?php
     require 'config.php';
     
-    $sql = "SELECT messages.content,users.username FROM messages INNER JOIN users on messages.userfrom = users.id ORDER BY messages.timestampp DESC";
+    $sql = "SELECT messages.content,users.username,users.idgrad FROM messages INNER JOIN users on messages.userfrom = users.id ORDER BY messages.timestampp DESC";
     $result = mysqli_query($conn, $sql);
     
     if (mysqli_num_rows($result) > 0) {
@@ -14,7 +14,13 @@
             if ($isImage) {//daca e path-ul catre o poza (deci daca contine jpg sau jpeg sau png)
                 echo "<p>" . $row["username"] . " <img src=uploads/". $row["content"]. " width=100 height=100>  </p>";
             }
-            echo "<p> ". $row["username"]. " : " . $row["content"]. "</p>";
+
+            if ($row["idgrad"] == 2) //mesaj trimis de moderator
+                echo '<p style="color: #d8b41f "> (Moderator) '. $row["username"]. " : " . $row["content"]. "</p>";
+            else if ($row["idgrad"] == 3) //mesaj trimis de admin
+                echo '<p style="color: #17eae4 "> (Admin) '. $row["username"]. " : " . $row["content"]. "</p>";
+            else 
+                echo "<p> ". $row["username"]. " : " . $row["content"]. "</p>";
         }
     } else {
     echo "0 results";
